dbcde9deb58ee98d1cf8c4ac77c217b4b5eb251f
aashish <ashwini.ashish@iqvia.com>
2021-05-24 21:18:21 +0530

    Enable home router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./screens/login/Login";
+import Home from './screens/home/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
+          <Route path="/home" component={Home} />
           <Route path="/" component={Login} />
         </Switch>
       </Router>
